9c095faf0808ad77e471c5e901755ec40501e676
Manish-Rawat-1186 <manishrawat1186@gmail.com>
2022-06-08 21:09:14 +0530

    second-commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../styles/Header.css'
+
+const Navbar = () => {
+  return (
+    <div className="navbar" >
+        <ul >
+            <li>
+                logo
+            </li>
+            <li>
+                about me 
+            </li>
+            <li>
+                project
+            </li>
+            <li>
+                social
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
